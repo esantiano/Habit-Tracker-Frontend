82660dc427cc7d6ebe824e9bda8846c6622eca2e
esantiano <103700795+esantiano@users.noreply.github.com>
2026-01-26 22:18:42 -0500

    cosmetic and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -16,35 +16,37 @@ export default function App() {
   const token = getToken();
 
   return (
-    <div style={{ maxWidth: 720, margin: "0 auto", padding: 16}}>
-      <header style={{ display: "flex", gap: 12, alignItems: "center", marginBottom: 16}}>
-        <Link to="/" style={{ fontWeight: 700, textDecoration: "none"}}>
-          Habit Tracker
-        </Link>
-        
-        <div style={{ marginLeft: "auto", display: "flex", gap: 12 }}>
-          {!token ? (
-            <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-            </>
-          ) : (
-            <button onClick={() => {
-              clearToken();
-              nav('/login');
-            }}
-            >Logout
-            </button>
-          )}
-        </div>
-      </header>
+    <div style={{ minHeight: "100vh"}}>
+      <div style={{ maxWidth: "720px", margin: "0 auto", padding: "20px 16px"}}>
+        <header style={{ display: "flex", gap: 12, alignItems: "center", marginBottom: 16}}>
+          <Link to="/" style={{ fontWeight: 700, textDecoration: "none"}}>
+            Habit Tracker
+          </Link>
+          
+          <div style={{ marginLeft: "auto", display: "flex", gap: 12 }}>
+            {!token ? (
+              <>
+              <Link className="centered" to="/login">Login</Link>
+              <Link className="centered" to="/register">Register</Link>
+              </>
+            ) : (
+              <button onClick={() => {
+                clearToken();
+                nav('/login');
+              }}
+              >Logout
+              </button>
+            )}
+          </div>
+        </header>
 
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/dashboard" element={<Protected><DashboardPage/></Protected>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/dashboard" element={<Protected><DashboardPage/></Protected>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
